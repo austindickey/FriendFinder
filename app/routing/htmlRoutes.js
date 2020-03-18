86015d4de365423664c9f240a29cfc84f488ce35
austindickey <austindickey@me.com>
2020-03-18 13:21:49 -0500

    adding the routing to html pages and listening on port 8000

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,9 @@
+// missing import code here
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"))
+})
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"))
+})
--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 var express = require("express")
 var path = require("path")
+
+var app = express()
+var port = 8000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"))
+})
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"))
+})
+
+app.listen(port, function() {
+    console.log("Server listening on port: " + port)
+})
