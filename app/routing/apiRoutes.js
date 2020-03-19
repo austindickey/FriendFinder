1d76f7517873338c2928515ff2c0b5011726ae1e
austindickey <austindickey@me.com>
2020-03-19 16:08:51 -0500

    moving friend data, adding math to api routes for each friend sum, adding on click file and logic for comparing score sums, adding script file tag to bottom of survey page

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,21 @@
-var path = require("path")
+var friendList = require("../data/friends.js")
+
+var allScores = []
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
-        
+        return res.json(friendList)
     })
     
     app.post("/api/friends", function(req, res) {
-        
+        allScores = []
+        for (let i = 0; i < friendList.length; i++) {
+            var calculating = friendList[i].scores
+            var scoreSum = calculating.reduce((a, b) => a + b, 0)
+            allScores.push(scoreSum)
+        }
+
+        module.exports = allScores
     })
 }
+
