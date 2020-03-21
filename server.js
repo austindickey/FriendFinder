311fe634e2ac5ea8447d6d451b62445116cad680
austindickey <austindickey@me.com>
2020-03-21 11:49:00 -0500

    changing the file structure and adding express.static() into the mix

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var port = 8000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.use('/static', express.static(path.join(__dirname + '/public')));
+
 require("./app/routing/htmlRoutes.js")(app)
 require("./app/routing/apiRoutes.js")(app)
 
