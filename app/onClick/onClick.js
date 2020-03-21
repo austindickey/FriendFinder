c0edd75c58d7ee73e4e22733210c360cf2edbb2a
austindickey <austindickey@me.com>
2020-03-20 22:05:42 -0500

    wrapping onclick js file in jquery ready function

--- app/onClick/onClick.js
+++ app/onClick/onClick.js
@@ -1,94 +1,96 @@
-var allScores = require("../routing/apiRoutes")
-var friendList = require("../data/friends.js")
+$(document).ready(function() {
+    var allScores = require("../routing/apiRoutes")
+    var friendList = require("../data/friends.js")
 
-var difference = []
+    var difference = []
 
-$("#submit").on("click", function(event) {
-    event.preventDefault()
-    console.log("working")
+    $("#submit").on("click", function(event) {
+        event.preventDefault()
+        console.log("working")
 
-    // Validating the data
-    function validateData() {
-        var isValid = true
-        $(".form-control").each(function () {
-            if ($(this).val() === "") {
-                isValid = false
-            }
-        })
+        // Validating the data
+        function validateData() {
+            var isValid = true
+            $(".form-control").each(function () {
+                if ($(this).val() === "") {
+                    isValid = false
+                }
+            })
 
-        $(".chosen-select").each(function () {
-            if ($(this).val() === "") {
-                isValid = false
-            }
-        })
-        return isValid
-    }
-
-    if (validateData()) {
-        // Grabbing the data
-        var newFriend = {
-            name: $("#name").val().trim(),
-            photo: $("#photo").val().trim(),
-            scores: [
-                $("#q1").val().trim(),
-                $("#q2").val().trim(),
-                $("#q3").val().trim(),
-                $("#q4").val().trim(),
-                $("#q5").val().trim(),
-                $("#q6").val().trim(),
-                $("#q7").val().trim(),
-                $("#q8").val().trim(),
-                $("#q9").val().trim(),
-                $("#q10").val().trim()
-            ]
-        }
-        
-        // calculate the new users score total
-        var newScores = newFriend.scores
-        var newScoresArray = []
-        for (let i = 0; i < newScores.length; i++) {
-            var scoreSum = newScores.reduce((a, b) => a + b, 0)
-            newScoresArray.push(scoreSum)
+            $(".chosen-select").each(function () {
+                if ($(this).val() === "") {
+                    isValid = false
+                }
+            })
+            return isValid
         }
 
-        for (let i = 0; i < allScores.length; i++) {
-            var smallestDiff = allScores[0] - newScoresArray[0]
-            var calDiff = allScores[i] - newScoresArray[0]
-
-            if (calDiff > 0 && calDiff < smallestDiff) {
-                smallestDiff = calDiff
-                difference.push(smallestDiff)
-                difference.push(i)
-            } else {
-                Math.abs(calDiff)
-                smallestDiff = calDiff
-                difference.push(smallestDiff)
-                difference.push(i)
+        if (validateData()) {
+            // Grabbing the data
+            var newFriend = {
+                name: $("#name").val().trim(),
+                photo: $("#photo").val().trim(),
+                scores: [
+                    $("#q1").val().trim(),
+                    $("#q2").val().trim(),
+                    $("#q3").val().trim(),
+                    $("#q4").val().trim(),
+                    $("#q5").val().trim(),
+                    $("#q6").val().trim(),
+                    $("#q7").val().trim(),
+                    $("#q8").val().trim(),
+                    $("#q9").val().trim(),
+                    $("#q10").val().trim()
+                ]
             }
+            
+            // calculate the new users score total
+            var newScores = newFriend.scores
+            var newScoresArray = []
+            for (let i = 0; i < newScores.length; i++) {
+                var scoreSum = newScores.reduce((a, b) => a + b, 0)
+                newScoresArray.push(scoreSum)
+            }
+
+            for (let i = 0; i < allScores.length; i++) {
+                var smallestDiff = allScores[0] - newScoresArray[0]
+                var calDiff = allScores[i] - newScoresArray[0]
+
+                if (calDiff > 0 && calDiff < smallestDiff) {
+                    smallestDiff = calDiff
+                    difference.push(smallestDiff)
+                    difference.push(i)
+                } else {
+                    Math.abs(calDiff)
+                    smallestDiff = calDiff
+                    difference.push(smallestDiff)
+                    difference.push(i)
+                }
+            }
+
+            // Displaying Best Match
+            var bestMatch = friendList[difference[1]]
+            $("#match-name").text(bestMatch.name)
+            $("#match-img").attr("src", bestMatch.photo)
+            $("#results-model").modal("toggle")
+
+            // Clearing the form
+            $("#name").val("")
+            $("#photo").val("")
+            $("#q1").val("")
+            $("#q2").val("")
+            $("#q3").val("")
+            $("#q4").val("")
+            $("#q5").val("")
+            $("#q6").val("")
+            $("#q7").val("")
+            $("#q8").val("")
+            $("#q9").val("")
+            $("#q10").val("")
+            
+        } else {
+            alert("Please fill out all of the fields before submitting!")
         }
 
-        // Displaying Best Match
-        var bestMatch = friendList[difference[1]]
-        $("#match-name").text(bestMatch.name)
-        $("#match-img").attr("src", bestMatch.photo)
-        $("#results-model").modal("toggle")
-
-        // Clearing the form
-        $("#name").val("")
-        $("#photo").val("")
-        $("#q1").val("")
-        $("#q2").val("")
-        $("#q3").val("")
-        $("#q4").val("")
-        $("#q5").val("")
-        $("#q6").val("")
-        $("#q7").val("")
-        $("#q8").val("")
-        $("#q9").val("")
-        $("#q10").val("")
-        
-    } else {
-        alert("Please fill out all of the fields before submitting!")
-    }
-
+    })
 })
